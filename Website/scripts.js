681a7cb9db8ae6f1d1c2f3ea36019c405f6e8118
KarthikeyaVasanthala <karthikeyavasanthala@gmail.com>
2024-05-30 19:33:52 +0530

    Add files via upload

---
+++ Website/scripts.js
@@ -0,0 +1,48 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     fetch('vehicles.json')
+//         .then(response => response.json())
+//         .then(data => {
+//             const vehicleContainer = document.getElementById('vehicle-container');
+//             data.vehicles.forEach(vehicle => {
+//                 const vehicleDiv = document.createElement('div');
+//                 vehicleDiv.className = 'vehicle';
+//                 vehicleDiv.onclick = () => redirectTo(vehicle.link);
+
+//                 const vehicleImage = document.createElement('img');
+//                 vehicleImage.src = vehicle.image;
+//                 vehicleImage.alt = vehicle.name;
+
+//                 const vehicleInfo = document.createElement('div');
+//                 vehicleInfo.className = 'vehicle-info';
+
+//                 const vehicleName = document.createElement('h2');
+//                 vehicleName.innerText = vehicle.name;
+
+//                 const vehiclePrice = document.createElement('p');
+//                 vehiclePrice.innerText = `${vehicle.price} MSRP* Starting At`;
+
+//                 const vehicleModels = document.createElement('p');
+//                 vehicleModels.innerText = vehicle.models;
+
+//                 vehicleInfo.appendChild(vehicleName);
+//                 vehicleInfo.appendChild(vehiclePrice);
+//                 vehicleInfo.appendChild(vehicleModels);
+
+//                 vehicleDiv.appendChild(vehicleImage);
+//                 vehicleDiv.appendChild(vehicleInfo);
+
+//                 vehicleContainer.appendChild(vehicleDiv);
+//             });
+//         });
+// });
+
+// function redirectTo(page) {
+//     window.location.href = page;
+// }
+
+function clickonyear()
+{
+    document.querySelector("")
+}
+
+
